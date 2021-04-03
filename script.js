6a52198d237f0e54505d1b82e7afe87e2c79ea2b
Salah Eddine Zemmouri <zemm.salaheddine@gmail.com>
2021-04-03 17:22:11 +0100

    added multicolor sketch and range input

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 // creating 16x16 grid
-const div = document.querySelector('.container');
+const div = document.querySelector('.gridContainer');
 // populate the container with 16*16 divs
 populate(16);
 // add hover effect
 addHoverAll();
 // add clear functionality
 clearAll();
+
+const current = document.querySelector('.range_input_value');
+const range = document.querySelector('#range_input');
+range.addEventListener('change',()=>{
+  let n = range.value;
+  console.log(n);
+  populate(n);
+  addHoverAll();
+  clearAll();
+  current.textContent = `${n}`;
+});
+
 
 function populate(size){
   div.textContent='';
@@ -23,10 +35,13 @@ function populate(size){
 }
 
 function addHoverAll(){
+
   const childs = Array.from(document.querySelectorAll('.gridElement'));
   childs.forEach( child => {
     child.addEventListener('mouseover',()=>{
       child.classList.add('hovered');
+      child.style.backgroundColor = 
+      `rgb(${Math.random()*266},${Math.random()*266},${Math.random()*266})`;
     });
   });
 }
@@ -37,9 +52,10 @@ function clearAll(){
   clearBtn.addEventListener('click',()=>{
     childs.forEach( child => {
       child.classList.remove('hovered');
+      child.style.backgroundColor = 'transparent';
     });
-    let n = Number(prompt('Enter the sketchpad size <100'));
-    populate(n);
-    addHoverAll();
   });
 }
+
+
+
